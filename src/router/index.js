bb6f11ecb21d550a210eaf877ff3a09363884e42
Marco Crapts <marco@WINKPN-J20P733.localdomain>
2020-09-20 22:52:28 +0200

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Home'
   },
   {
     path: '/about',
